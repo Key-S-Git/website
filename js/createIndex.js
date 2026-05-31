ef7b98759b87387f177a6a06e8ef18dbe33e35da
Key-S <0rp32554022302d@gmail.com>
2026-06-01 07:25:12 +0900

    Update createIndex.js

--- js/createIndex.js
+++ js/createIndex.js
@@ -1,75 +1,60 @@
-// Gunpla の動的生成
-fetch('json/gunpla.json')
-  .then(res => res.json())
-  .then(allData => {
-    const container = document.getElementById('gunpla-container');
-    if (!container) return;
+/**
+ * 各カテゴリーのデータを取得して画面に描画する共通関数
+ * @param {String} type - 項目のジャンル（gunpla, travelogue, recipe）
+ * @param {Function} parseData - 各データから[タイトル, サブタイトル]を抽出する関数
+ */
+function loadAndRenderCategory(type, parseData) {
 
-    Object.keys(allData).forEach(id => {
-      const data = allData[id];
-      // サムネイル表示用に名前を改行させる
-      const displayName = data.name.replace(' ', '<br>');
+  // type から自動的に JSON のパスを生成
+  fetch(`json/${type}.json`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Fetch error for ${type}`);
+      return res.json();
+    })
+    .then(allData => {
+      Object.keys(allData).forEach(id => {
+        const data = allData[id];
+        const { title, sub } = parseData(data);
+        renderItem(type, id, data.thumbnail, title, sub);
+      });
+    })
+    .catch(err => console.error(`Failed to load ${type}:`, err));
+}
 
-      const item = `
-      <div class="container">
-            <a href="gunpla.html?id=${id}" class="album-item">
-                <div class="box"><img src="${data.thumbnail}"></div>
-                <div class="overlay">
-                    <div class="title">${displayName}</div>
-                    <div class="sub">${data.number}</div>
-                </div>
-            </a>
-        </div>
-      `;
-      container.insertAdjacentHTML('beforeend', item);
-    });
-  });
+/**
+ * JSONで取得した各データからHTML要素を作成する関数
+ */
+function renderItem(type, id, thumbnail, title, sub) {
+  const item = `
+    <div class="container">
+        <a href="${type}.html?id=${id}" class="album-item">
+            <div class="box"><img src="${thumbnail}"></div>
+            <div class="overlay">
+                <div class="title">${title}</div>
+                <div class="sub">${sub}</div>
+            </div>
+        </a>
+    </div>
+  `;
+  const container = document.getElementById(`${type}-container`);
+  container.insertAdjacentHTML('beforeend', item);
+}
 
-// Traveloguesの動的生成
-fetch('json/travelogue.json')
-  .then(res => res.json())
-  .then(allData => {
-    const container = document.getElementById('travelogue-container');
-    if (!container) return;
 
-    Object.keys(allData).forEach(id => {
-      const data = allData[id];
+// Gunpla
+loadAndRenderCategory('gunpla', data => ({
+  title: data.name.replace(' ', '<br>'), // 機体名の半角スペースを改行に置き換えて主題とする
+  sub: data.number // 型番を副題とする
+}));
 
-      const item = `
-          <div class="container">
-              <a href="travelogue.html?id=${id}" class="album-item">
-                  <div class="box"><img src="${data.thumbnail}"></div>
-                  <div class="overlay">
-                      <div class="title">${data.title}</div>
-                      <div class="sub">${data.date}</div>
-                  </div>
-              </a>
-          </div>
-      `;
-      container.insertAdjacentHTML('beforeend', item);
-    });
-  });
+// Travelogues
+loadAndRenderCategory('travelogue', data => ({
+  title: data.title, // 旅行先の名前を主題とする
+  sub: data.date // 日付を副題とする
+}));
 
-// Recipes の動的生成
-fetch('json/recipe.json')
-  .then(res => res.json())
-  .then(allData => {
-    const container = document.getElementById('recipe-container');
-    if (!container) return;
-
-    Object.keys(allData).forEach(id => {
-      const data = allData[id];
-
-      const item = `
-        <div class="container">
-            <a href="recipe.html?id=${id}" class="album-item">
-                <div class="box"><img src="${data.thumbnail}"></div>
-                <div class="overlay">
-                    <div class="title">${data.title}</div>
-                </div>
-            </a>
-        </div>
-      `;
-      container.insertAdjacentHTML('beforeend', item);
-    });
-  });
+// Recipes
+loadAndRenderCategory('recipe', data => ({
+  title: data.title, // 料理名を主題とする
+  sub: '' // 副題はなし
+}));
