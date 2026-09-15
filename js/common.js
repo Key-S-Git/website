997aeb4966a06e509998bf7bb4da977974bce58d
Key-S <0rp32554022302d@gmail.com>
2026-09-15 20:14:00 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,33 @@
+/**
+ * 404ページに遷移させる関数
+ */
+export function redirectTo404() {
+  window.location.href = '/404.html';
+}
+
+/**
+ * jsonからデータを取得する関数
+ * @param {*} jsonPath 
+ * @param {*} id 
+ * @returns 
+ */
+export async function fetchDataById(jsonPath, id) {
+  if (!id) {
+    redirectTo404();
+    return null;
+  }
+  try {
+    const res = await fetch(jsonPath);
+    const allData = await res.json();
+    const data = allData[id];
+    if (!data) {
+      redirectTo404();
+      return null;
+    }
+    return data;
+  } catch (err) {
+    console.error(err);
+    redirectTo404();
+    return null;
+  }
+}
